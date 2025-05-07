979c9d98f182b90a0c1a367fa41cf1a5423f8494
heesang.lee <sky1994601@hanmail.net>
2025-05-07 21:02:14 +0900

    fix: github action 변경

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1746614681',resources: [
+      '/assets/css/.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
